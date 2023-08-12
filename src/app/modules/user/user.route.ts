8d60d2988af07d40d487fde18c9e7c04c4cba977
arifmia1129 <arifmia1129@gmail.com>
2023-08-12 08:03:32 +0600

    user router

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,1 +1,15 @@
+import { Router } from "express";
+import * as userController from "./user.controller";
+import requestValidator from "../../middleware/requestValidator";
+import { updateUserValidation } from "./user.validation";
 
+const userRouter = Router();
+
+userRouter.get("/", userController.getUser);
+userRouter
+  .route("/:id")
+  .get(userController.getUserById)
+  .patch(requestValidator(updateUserValidation), userController.updateUserById)
+  .delete(userController.deleteUserById);
+
+export default userRouter;
