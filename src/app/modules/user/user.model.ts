410ef8548060b2acfa5758bf97370803ee13ecf5
arifmia1129 <arifmia1129@gmail.com>
2023-08-12 06:57:33 +0600

    [DONE] user model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+import { IUser, IUserMethods, UserModel } from "./user.interface";
+import { userRole } from "./user.constant";
+
+const userSchema = new Schema<IUser, UserModel, IUserMethods>(
+  {
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: userRole,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value: string): boolean {
+          return value.length === 6;
+        },
+        message: "Password must be 6 character",
+      },
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+      },
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    budget: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    income: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = model<IUser, UserModel>("User", userSchema);
+
+export default User;
