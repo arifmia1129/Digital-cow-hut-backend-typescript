94c6a36421c9adde9025d1cd16cac4bd99feb671
arifmia1129 <arifmia1129@gmail.com>
2023-08-12 08:25:22 +0600

    cow constant created

---
+++ src/app/modules/cow/cow.constant.ts
@@ -0,0 +1,45 @@
+export const cowLocation = [
+  "Dhaka",
+  "Chattogram",
+  "Barishal",
+  "Rajshahi",
+  "Sylhet",
+  "Comilla",
+  "Rangpur",
+  "Mymensingh",
+];
+
+export const cowBread = [
+  "Brahman",
+  "Nellore",
+  "Sahiwal",
+  "Gir",
+  "Indigenous",
+  "Tharparkar",
+];
+
+export const cowLabel = ["for sale", "sold out"];
+
+export const cowCategory = ["Dairy", "Beef", "DualPurpose"];
+
+export const cowSearchableField = [
+  "name",
+  "age",
+  "price",
+  "location",
+  "bread",
+  "weight",
+  "label",
+  "category",
+];
+
+export const cowFilterableField = [
+  "name",
+  "age",
+  "price",
+  "location",
+  "bread",
+  "weight",
+  "label",
+  "category",
+];
