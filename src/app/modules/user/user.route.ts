7863831ce769f8b6bb87983528d344e37e9a1b84
arifmia1129 <arifmia1129@gmail.com>
2023-08-14 12:39:02 +0600

    user api auth guard added by auth middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import * as userController from "./user.controller";
 import requestValidator from "../../middleware/requestValidator";
 import { updateUserValidation } from "./user.validation";
+import auth from "../../middleware/auth";
+import { USER_ROLE_ENUM } from "../../../enums/user.enum";
 
 const userRouter = Router();
 
-userRouter.get("/", userController.getUser);
+userRouter.get("/", auth(USER_ROLE_ENUM.ADMIN), userController.getUser);
 userRouter
   .route("/:id")
+  .all(auth(USER_ROLE_ENUM.ADMIN))
   .get(userController.getUserById)
   .patch(requestValidator(updateUserValidation), userController.updateUserById)
   .delete(userController.deleteUserById);
--- src/enums/user.enum.ts
+++ src/enums/user.enum.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 export enum USER_ROLE_ENUM {
-  SUPER_ADMIN = "super_admin",
   ADMIN = "admin",
-  STUDENT = "student",
-  FACULTY = "faculty",
+  SELLER = "seller",
+  BUYER = "buyer",
 }
